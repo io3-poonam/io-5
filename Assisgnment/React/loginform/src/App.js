f60ba22dccf9c1b536031f163dcf7bc1bae26b99
Poonam Maurya <io3.poonam30@gmail.com>
2023-12-02 19:30:59 +0530

    login form

---
+++ Assisgnment/React/loginform/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './component/Header/Header';
+import Description from './component/Desc/Description';
+import LoginPage from './component/LoginPageBox/LoginPage';
+import FooterSide from './component/FooterSide/FooterUpper';
+import Footer from './component/Footer/Footer';
+function App() {
+  return (
+    <>
+    <Header/>
+    <Description/>
+    <LoginPage/>
+    <FooterSide/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ Assisgnment/React/loginform/src/component/FooterSide/FooterUpper.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css"
+function FooterSide(){
+    return(
+<>
+<div className="FooterSide">
+    <ul className="footerUl">
+        <li className="liTagFooter">Home</li>
+        <li className="liTagFooter">Support</li>
+        <li className="liTagFooter">Contact</li>
+
+    </ul>
+</div>
+</>)
+}
+export default FooterSide;
